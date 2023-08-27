3323779c0403e7d9a8ba08f7faac7a1d96d0506f
elchakal23 <moneyemoji.pro@gmail.com>
2023-08-27 21:55:44 +0200

    first commi3sd2

--- components/Bulb.js
+++ components/Bulb.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 const Bulb = () => {
   return (
     <div className="absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-100 z-10 w-[240px] xl-w-[200px]">
-      <Image src={'/bulb.png'} width={260} height={200} className="h-full w-full" />
+      <Image src={'/bulb.png'} alt="" width={260} height={200} className="h-full w-full" />
     </div>
   )
 }
