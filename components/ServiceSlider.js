84298cc804b348563b3366942c77fe667ae867c1
elchakal23 <moneyemoji.pro@gmail.com>
2023-08-29 19:17:37 +0200

    first commi3sdsddasdsadasddadadadasdsasda2

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -19,7 +19,6 @@ import { LuRepeat2} from "react-icons/lu";
 
 
 import { FreeMode, Pagination } from 'swiper'
-import index from '../pages/services'
 
 const serviceData = [
   {
@@ -54,48 +53,42 @@ const serviceData = [
   },
 ];
 
-
-
-
-
 const ServiceSlider = () => {
-  return (
-    <Swiper breakpoints={{
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 15,
-      },
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 15,
-      },
+  return <Swiper breakpoints={{
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 15
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 15
+    },
+    
+  }}
+  freeMode={true}
+  pagination={{
+    clickable: true
+  }}
+  modules={[FreeMode, Pagination]}
+  className='h-[240px] sm:h-[340px]'
+  >
+    {
+      serviceData.map((item,index) => {
+        return <SwiperSlide key={index}> 
+        <div className='bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 '> 
+          <div className='text-4xl text-accent mb-4'>{item.icon}</div>
+          <div className='mb-8'>
+            <div className='mb-2 text-lg'>{item.title}</div>
+            <p className='max-w-[350px] leading-normal'>{item.description}</p>
+          </div>
+          <div className='text-3xl'>
+            <RxArrowTopRight className='group-hover:rotate-45 group-hover:text-accent transition-all duration-300' />
+          </div>
+        </div>
+        </SwiperSlide>
+      })
+    }
+  </Swiper>;
+};
 
-    }}
-    freeMode={true}
-    pagination={{
-      clickable: true,
-    }}
-    modules={[FreeMode, Pagination]}
-    className='h-[240px] sm:h-[340px]'
-    >
-      {
-        serviceData.map((item,index) => {
-          return <SwiperSlide key={index}>
-            <div>
-              <div>{item.icon}</div>
-              <div>
-                {item.title}
-                <p>{item.description}</p>
-              </div>
-              <div className='text-3xl'>
-                <RxArrowTopRight />
-              </div>
-            </div>
-          </SwiperSlide>
-        })
-      }
-    </Swiper>
-  )
-}
-
-export default ServiceSlider
+export default ServiceSlider;
--- components/Transition.js
+++ components/Transition.js
@@ -22,7 +22,7 @@ const Transition = () => {
   return (
     <>
     <motion.div
-      className='fixed top-0 bottom-0 right-full h-screen z-30 bg-[#2e2257]'
+      className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]'
       variants={transitionVariants}
       initial='initial'
       animate='animate'
@@ -30,7 +30,7 @@ const Transition = () => {
       transition={{ delay: 0.2, duration: 0.6, ease: 'easeInOut' }}
     >
        <motion.div
-      className='fixed top-0 bottom-0 right-full h-screen z-20 bg-[#2e2257]'
+      className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#2e2257]'
       variants={transitionVariants}
       initial='initial'
       animate='animate'
@@ -38,7 +38,7 @@ const Transition = () => {
       transition={{ delay: 0.4, duration: 0.6, ease: 'easeInOut' }}
     ></motion.div>
      <motion.div
-      className='fixed top-0 bottom-0 right-full h-screen z-10 bg-[#2e2257]'
+      className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#2e2257]'
       variants={transitionVariants}
       initial='initial'
       animate='animate'
