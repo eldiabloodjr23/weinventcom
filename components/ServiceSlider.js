9e35bb791ba75ca524d5ca6a31c50a41066d6cf0
elchakal23 <moneyemoji.pro@gmail.com>
2023-08-29 15:26:29 +0200

    first commi3sdsddasdsadasddadadadasdsasda2

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -72,7 +72,7 @@ const ServiceSlider = () => {
     clickable: true
   }}
   modules={[FreeMode, Pagination]}
-  className='h-[150px] sm:h-[140px]'
+  className='h-[240px] sm:h-[340px]'
   >
     {
       serviceData.map((item,index) => {
