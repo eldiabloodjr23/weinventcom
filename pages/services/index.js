5856ea7ae94a8add2e21c1a94e42647f8b77ed1c
elchakal23 <moneyemoji.pro@gmail.com>
2023-08-29 15:03:49 +0200

    first commi3sdsddasdsadasddadadadasdsa2

--- pages/services/index.js
+++ pages/services/index.js
@@ -11,7 +11,7 @@ const index = () => {
   console.log(ServiceSlider);
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center">
-     <Circles/>
+    
       <div className="container mx-auto">
       <div className="flex flex-col xl:flex-row gap-x-8">
         <div className="text-center  flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
@@ -27,7 +27,7 @@ const index = () => {
       
       </div>
       
-      <Bulb />
+     
       
     </div>
   )
